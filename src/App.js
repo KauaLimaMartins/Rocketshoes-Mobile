0ebb2d978c32c5196b9826e0bc9e2dfd580bfbad
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-25 15:00:33 -0300

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import 'react-native-gesture-handler';
+import './config/Reactotron';
 
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Routes from './routes';
 
 export default function App() {
-  return <View></View>;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
