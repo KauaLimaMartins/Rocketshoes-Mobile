9ff783be1e7988b2b6c5e7fd4e3b978b46f726b2
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-29 09:28:45 -0300

    Configure routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './screens/Home';
@@ -9,11 +8,9 @@ const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" headerMode="none">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Cart" component={Cart} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Home" headerMode="none">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Cart" component={Cart} />
+    </Stack.Navigator>
   );
 }
