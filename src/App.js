88fe16fdceda9d5cc59102f4e3c146c5e72a21e3
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-29 09:28:30 -0300

    Import Intl

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import 'react-native-gesture-handler';
 import './config/Reactotron';
 
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Header from './components/Header';
 
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#191920" />
+      <Header />
       <Routes />
-    </>
+    </NavigationContainer>
   );
 }
