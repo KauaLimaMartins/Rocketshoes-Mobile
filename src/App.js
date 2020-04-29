86fefc04bcbf56b927c8863b3bf8a34f59d2243b
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-04-29 12:35:03 -0300

    Change Header component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Header from './components/Header';
-
 import Routes from './routes';
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#191920" />
-      <Header />
       <Routes />
     </NavigationContainer>
   );
