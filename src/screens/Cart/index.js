eeddd42a112feef3b8f60c4768b41fabdd93ba62
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-05-01 13:01:33 -0300

    Configure Cart

--- src/screens/Cart/index.js
+++ src/screens/Cart/index.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/Entypo';
+import { FlatList } from 'react-native';
 
 import Header from '../../components/Header';
+import {
+  Container,
+  CartContainer,
+  ProductPrice,
+  ProductTitle,
+  ProductImage,
+  ProductInformationsContainer,
+  RemoveProduct,
+  ProductQuantity,
+  ProductContainer,
+} from './styles';
 
-export default class Cart extends Component {
-  render() {
-    return <Header home="Home" cart="Cart" />;
-  }
+function Cart({ products }) {
+  return (
+    <>
+      <Header home="Home" cart="Cart" />
+      <Container>
+        <CartContainer>
+          <FlatList
+            data={products}
+            keyExtractor={(product) => String(product.id)}
+            renderItem={({ item }) => (
+              <ProductContainer>
+                <ProductImage source={{ uri: item.image }} />
+                <ProductInformationsContainer>
+                  <ProductTitle>{item.title}</ProductTitle>
+                  <ProductPrice>{item.formattedPrice}</ProductPrice>
+                </ProductInformationsContainer>
+                <ProductQuantity>2</ProductQuantity>
+                <RemoveProduct>
+                  <Icon name="trash" color="#7159c1" size={26} />
+                </RemoveProduct>
+              </ProductContainer>
+            )}
+          />
+        </CartContainer>
+      </Container>
+    </>
+  );
 }
+
+const mapStateToProps = (state) => ({
+  products: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
--- src/screens/Cart/styles.js
+++ src/screens/Cart/styles.js
@@ -5,3 +5,49 @@ export const Container = styled.View`
   padding: 20px;
   background: #191920;
 `;
+
+export const CartContainer = styled.View`
+  background: #fff;
+  width: 100%;
+  border-radius: 10px;
+`;
+
+export const ProductContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  margin-left: 5px;
+`;
+
+export const ProductInformationsContainer = styled.View`
+  width: 170px;
+  margin-left: 10px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #222;
+`;
+
+export const ProductQuantity = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  margin-right: 20px;
+  background: #ededed;
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+export const RemoveProduct = styled.TouchableOpacity``;
