0f54769ec958baa0c975213f9640cf0a07c5a4b2
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-05-01 16:03:47 -0300

    Remove product action

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -4,10 +4,24 @@ export function addToCartRequest(id) {
     id,
   };
 }
-
 export function addToCartSuccess(product) {
   return {
     type: '@cart/ADD_SUCCESS',
     product,
+  };
+}
+
+export function updateAmountSuccess(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT_SUCCESS',
+    id,
+    amount,
+  };
+}
+
+export function removeFromCartSuccess(id) {
+  return {
+    type: '@cart/REMOVE_SUCCESS',
+    id,
   };
 }
