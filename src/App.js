cc86c13c97960fb0bee41978fc66e4622336956a
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-05-01 13:00:51 -0300

    Add provider to redux

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import 'react-native-gesture-handler';
 import './config/Reactotron';
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
+import store from './store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#191920" />
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor="#191920" />
+        <Routes />
+      </NavigationContainer>
+    </Provider>
   );
 }
